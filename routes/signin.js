0f1619c76d8f44abab156020198909507d967fd2
sitrongtrang <sitrongtrang@gmail.com>
2023-12-02 09:54:36 +0700

    merge nhat

--- routes/signin.js
+++ routes/signin.js
@@ -3,9 +3,5 @@ const signin_router = express.Router();
 const signin_controller = require('../controllers/signin');
 
 signin_router.post("/", signin_controller.signin);
-signin_router.get("/", function(req,res){
-    console.log("in backend");
-    res.status(200).json({data: "helloworld"})
-})
 
 module.exports = signin_router;

af690af3acb9c184fa8b791de9c8969e9c88785f
sitrongtrang <sitrongtrang@gmail.com>
2023-12-03 14:39:56 +0700

    added mock data

--- models/authentication.js
+++ models/authentication.js
@@ -15,37 +15,37 @@ function checkNoEmpty(obj) {
 }
 
 function signin(res, obj) {
-  // connect_DB.query("SELECT * FROM members WHERE email = ?", [obj.email], function (err, result, field) {
-  //   if (err) {
-  //     return res.status(500).json({ message: "Hệ thống gặp vấn đề. Vui lòng thử lại sau" });
-  //   }
+  connect_DB.query("SELECT * FROM members WHERE email = ?", [obj.email], function (err, result, field) {
+    if (err) {
+      return res.status(500).json({ message: "Hệ thống gặp vấn đề. Vui lòng thử lại sau" });
+    }
     
-  //   if (result.length == 0) {
-  //     return res.status(400).json({ message: "Người dùng không tồn tại hoặc sai thông tin đăng nhập. Vui lòng thử lại" });
-  //   }
+    if (result.length == 0) {
+      return res.status(400).json({ message: "Người dùng không tồn tại hoặc sai thông tin đăng nhập. Vui lòng thử lại" });
+    }
     
-  //   bcrypt
-  //     .compare(obj.password, result[0].password)
-  //     .then((passwordCheck) => {
-  //       if (!passwordCheck) {
-  //         res.status(401).json({ message: "Người dùng không tồn tại hoặc sai thông tin đăng nhập. Vui lòng thử lại" });
-  //       }
-  //       else {
-  //         const member = {
-  //             student_id: result[0].student_id,
-  //             email: result[0].email,
-  //             state: result[0].state,
-  //             permission: result[0].permission
-  //         };
-  //         const token = jwt.sign(member, "RANDOM-TOKEN", { expiresIn: "10m" });
-  //         res.json({ member: member, token: token });
-  //       }
-  //     })
-  //     .catch((error) => {
-  //         res.status(500).json({ message: "Hệ thống gặp vấn đề. Vui lòng thử lại sau" });
-  //     });
-  // })
-  res.json({ data: JSON.stringify("Logged in") });
+    bcrypt
+      .compare(obj.password, result[0].password)
+      .then((passwordCheck) => {
+        if (!passwordCheck) {
+          res.status(401).json({ message: "Người dùng không tồn tại hoặc sai thông tin đăng nhập. Vui lòng thử lại" });
+        }
+        else {
+          const member = {
+              student_id: result[0].student_id,
+              email: result[0].email,
+              state: result[0].state,
+              permission: result[0].permission
+          };
+          const token = jwt.sign(member, "RANDOM-TOKEN", { expiresIn: "10m" });
+          res.json({ member: member, token: token });
+        }
+      })
+      .catch((error) => {
+          res.status(500).json({ message: "Hệ thống gặp vấn đề. Vui lòng thử lại sau" });
+      });
+  })
+  //res.json({ data: JSON.stringify("Logged in") });
 }
 
 module.exports = { checkNoEmpty, signin }
